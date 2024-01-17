2aac3fd807221d21dae28109b6813260d81adba2
Aditya <58824912+TriStaRvOiD@users.noreply.github.com>
2024-01-17 12:24:22 +0530

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,15 +37,14 @@ android {
 
 dependencies {
 
-    //mxparser
     implementation("org.mariuszgromada.math:MathParser.org-mXparser:5.2.1")
     
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.gridlayout:gridlayout:1.1.0-beta01")
-    implementation("androidx.navigation:navigation-fragment:2.7.5")
-    implementation("androidx.navigation:navigation-ui:2.7.5")
+    implementation("androidx.navigation:navigation-fragment:2.7.6")
+    implementation("androidx.navigation:navigation-ui:2.7.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
